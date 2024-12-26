2570b3b3e5a0c8a8174381751e3a16c198486add
cristobalnextservice <cristobal.palma@nextservicechile.cl>
2024-12-26 13:07:15 -0300

    examples

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { PokemonComponent } from './pokemon/pokemon.component';
-import { FintocComponent } from './fintoc/fintoc.component';
 
 // export const routes: Routes = [
 //   { path: 'home', component: HomeComponent },
@@ -15,18 +12,6 @@ import { FintocComponent } from './fintoc/fintoc.component';
 // ];
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
-  },
-  {
-    path: 'pokemon',
-    loadComponent: () => import('./pokemon/pokemon.component').then((m) => m.PokemonComponent),
-  },
-  {
-    path: 'fintoc',
-    loadComponent: () => import('./fintoc/fintoc.component').then((m) => m.FintocComponent),
-  },
   {
     path: 'forms',
     loadComponent: () => import('./forms/forms.component').then((m) => m.FormsComponent),
